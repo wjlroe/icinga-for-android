94e2fb2c2d4e13ed85bc6b0a9d460fd6ee810dc6
William Roe <william.roe@migcan.com>
2011-01-17 17:08:48 +0000

    Initial empty Android Project

---
+++ src/uk/org/wjlr/android/icinga/Icinga.java
@@ -0,0 +1,15 @@
+package uk.org.wjlr.android.icinga;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Icinga extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
